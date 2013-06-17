718eb657e756d30130d00b4b0c92599d9adb9cbd
Winter Guerra <mr.servergeek@gmail.com>
2013-06-17 14:23:35 -0400

    Slowed down the RPM and tuned the LED on time (for my specific zoetrope application.)

--- zoetrope/config.h
+++ zoetrope/config.h
@@ -21,7 +21,7 @@ const uint16_t motorStepsPerRotation = 200;
 // *Microstepping Devider, set to the denominator of your stepping setup fraction
 // *I.E. 1 for whole stepping, 2 for half stepping, 4 for quarter stepping Etc, Etc....
 // ****
-const uint16_t microsteppingDevider = 1;
+const uint16_t microsteppingDevider = 2;
 
 // ****
 // *IO pin definitions
@@ -53,17 +53,17 @@ const uint8_t frameCount = 15;
 // ****
 // *Animation spindle rotation speed. 80 Seems to be the maximum for a whole stepping motor
 // ****
-const uint8_t targetRPM = 80;
+const uint8_t targetRPM = 60;
 
 // Set this to "true" if you're building a new zoetrope. The frame devider is a very course way of dealing with the LED on time, but can be very useful for ballparking the final on time. After ballparking, we recommend switching over to manual control of the 'StepsToFlash' variable. 
 // NOTE: To find the StepsToFlash value for a paticular framedevider, switch to DEBUG mode (setting below) and use the serially printed value for the 'StepsToFlash' var.
-boolean Use_Frame_Devider = true;
+boolean Use_Frame_Devider = false;
 
 // Defines what fraction of the frame should the LED be on for (Default 30 or 1/30 the time of the whole frame)
-uint16_t frameDevider = 3; //Change this to make the leds flash for a shorter devision of the frame
+uint16_t frameDevider = 5; //Change this to make the leds flash for a shorter devision of the frame
 
 // Defines how many steps should the LED be on for beginning at the start of the animation frame. Use this variable for finer control once you've ballparked the value using both the frameDevider variable and the DEBUG mode output.
-uint16_t StepsToFlash; 
+uint16_t StepsToFlash = 1; 
 
 // DEBUG FUNCTION VARIABLES
 
